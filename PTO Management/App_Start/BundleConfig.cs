ee065c742fa61604bcb5b5a680e188c3e7396b7e
ccuellar1 <39417094+ccuellar1@users.noreply.github.com>
2018-05-21 13:07:22 -0400

    Add files via upload

---
+++ PTO Management/App_Start/BundleConfig.cs
@@ -0,0 +1,40 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace PTO_Management
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js",
+                      "~/Scripts/script.js",
+                      "~/Scripts/lib/cupertino/jquery-ui.min.css",
+                      "~/Scripts/lib/moment.min.js",
+                      "~/Scripts/moment.js",
+                      "~/Scripts/fullcalendar.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css",
+                      "~/Content/PTOStyle.css",
+                       "~/Content/icons.css",
+                       "~/Content/fullcalendar.min.css",
+                       "~/Content/fullcalendar.css"));
+        }
+    }
+}
